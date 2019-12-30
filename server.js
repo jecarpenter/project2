153dd19936b40f20067d58394f3bc3d87a4b4982
Daniel Groveman <daniel5075@gmail.com>
2019-12-29 21:40:32 -0600

    started login logic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var path = require("path");
 var db = require("./models");
 
 var app = express();
@@ -34,8 +34,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
