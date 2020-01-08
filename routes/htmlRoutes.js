878dfd4651e91962ba0ec74fa9693db55731a440
Daniel Groveman <daniel5075@gmail.com>
2020-01-07 18:34:08 -0600

    updated htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,20 +3,21 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-
-    res.render("register", {
-      msg: "Welcome!",
-
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
     });
-
   });
-  app.get("/login", function (req, res) {
 
-    res.render("login", {
-      msg: "Welcome!",
-
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
     });
-
   });
 
   // Load example page and pass in an example by id
