e7f88ac302f2c198a70e389307d3c368610f79c9
Daniel Groveman <daniel5075@gmail.com>
2020-01-11 10:29:20 -0600

    updated devregister, moved images into public

---
+++ routes/devregister-api-routes.js
@@ -0,0 +1,41 @@
+var db = require("../models");
+
+module.exports = function (app) {
+  // Find all Authors and return them to the user with res.json
+  app.get("/api/devregisters", function (req, res) {
+    db.profiles.findAll({}).then(function (dbProfiles) {
+      res.json(dbProfiles);
+    });
+  });
+
+  app.get("/api/devregisters/:id", function (req, res) {
+
+    db.profiles.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbProfiles) {
+      res.json(dbProfiles);
+    });
+  });
+
+  app.post("/api/devregisters", function (req, res) {
+    // Create an Author with the data available to us in req.body
+    console.log(req.body);
+    db.profiles.create(req.body).then(function (dbProfiles) {
+      res.json(dbProfiles);
+    });
+  });
+
+  app.delete("/api/devregisters/:id", function (req, res) {
+    // Delete the Author with the id available to us in req.params.id
+    db.profiles.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbProfiles) {
+      res.json(dbProfiles);
+    });
+  });
+
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function (app) {
   // Load index page
