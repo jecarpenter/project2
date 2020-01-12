287e3619eb1734054d0ddacc5b4286e18206be3f
Daniel Groveman <daniel5075@gmail.com>
2020-01-12 15:42:47 -0600

    testing

--- server.js
+++ server.js
@@ -43,61 +43,61 @@ db.sequelize.sync({ force: false }).then(function () {
 //login logic begins here:
 
 //setting up body parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.post('/register', (req, res) => {
-  res.status(200).send({ access_token: '' });
-});
+// app.post('/register', (req, res) => {
+//   res.status(200).send({ access_token: '' });
+// });
 
-app.post('/login', (req, res) => {
-  res.status(200).send({ access_token: '' });
-});
+// app.post('/login', (req, res) => {
+//   res.status(200).send({ access_token: '' });
+// });
 
 //setting the homepage and displaying text to test connection
-app.get('/', (req, res) => {
-  res.status(200).send('This is an authentication server');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('This is an authentication server');
+// });
 
-//Here is the post method for sending new user data to the database
-app.post('/register', (req, res) => {
+// Here is the post method for sending new user data to the database
+// app.post('/register', (req, res) => {
 
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = bcrypt.hashSync(req.body.password);
+//   const name = req.body.name;
+//   const email = req.body.email;
+//   const password = bcrypt.hashSync(req.body.password);
 
-  createUser([name, email, password], (err) => {
-    if (err) return res.status(500).send("Server error!");
-    findUserByEmail(email, (err, user) => {
-      if (err) return res.status(500).send('Server error!');
-      const expiresIn = 24 * 60 * 60;
-      const accessToken = jwt.sign({ id: user.id }, SECRET_KEY, {
-        expiresIn: expiresIn
-      });
-      res.status(200).send({
-        "user": user, "access_token": accessToken, "expires_in": expiresIn
-      });
-    });
-  });
-});
+//   createUser([name, email, password], (err) => {
+//     if (err) return res.status(500).send("Server error!");
+//     findUserByEmail(email, (err, user) => {
+//       if (err) return res.status(500).send('Server error!');
+//       const expiresIn = 24 * 60 * 60;
+//       const accessToken = jwt.sign({ id: user.id }, SECRET_KEY, {
+//         expiresIn: expiresIn
+//       });
+//       res.status(200).send({
+//         "user": user, "access_token": accessToken, "expires_in": expiresIn
+//       });
+//     });
+//   });
+// });
 
 //setting up login post for database
-app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  findUserByEmail(email, (err, user) => {
-    if (err) return res.status(500).send('Server error!');
-    if (!user) return res.status(404).send('User not found!');
-    const result = bcrypt.compareSync(password, user.password);
-    if (!result) return res.status(401).send('Password not valid!');
+// app.post('/login', (req, res) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   findUserByEmail(email, (err, user) => {
+//     if (err) return res.status(500).send('Server error!');
+//     if (!user) return res.status(404).send('User not found!');
+//     const result = bcrypt.compareSync(password, user.password);
+//     if (!result) return res.status(401).send('Password not valid!');
 
-    const expiresIn = 24 * 60 * 60;
-    const accessToken = jwt.sign({ id: user.id }, SECRET_KEY, {
-      expiresIn: expiresIn
-    });
-    res.status(200).send({ "user": user, "access_token": accessToken, "expires_in": expiresIn });
-  });
-});
+//     const expiresIn = 24 * 60 * 60;
+//     const accessToken = jwt.sign({ id: user.id }, SECRET_KEY, {
+//       expiresIn: expiresIn
+//     });
+//     res.status(200).send({ "user": user, "access_token": accessToken, "expires_in": expiresIn });
+//   });
+// });
 
 
 module.exports = app;
